9e4f503a003b60d12b74e61bff69bfe8e5e8286e
Roman Iten <roman.iten@astlo.ch>
2017-01-17 13:48:04 +0100

    fixup! libports: add gtest (the Google C++ testing framework)

--- repos/libports/src/test/libports/gtest/target.mk
+++ repos/libports/src/test/libports/gtest/target.mk
@@ -4,7 +4,10 @@ GTEST_DIR := $(call select_from_ports,googletest)/src/lib/googletest/googletest
 
 INC_DIR += $(GTEST_DIR)
 
-SRC_CC = $(GTEST_DIR)/src/gtest_main.cc
-SRC_CC += $(GTEST_DIR)/test/gtest_all_test.cc
+SRC_CC = gtest_main.cc gtest_all_test.cc
 
 LIBS = config_args stdcxx gtest
+
+vpath gtest_main.cc $(GTEST_DIR)/src
+vpath gtest_all_test.cc $(GTEST_DIR)/test
+
