a6989f0a4beaf48bba8ad96383085910ba0e2c0e
Christian Helmuth <christian.helmuth@genode-labs.com>
2017-09-19 12:27:14 +0200

    lxip: fix timer handling and jiffies update

--- repos/dde_linux/src/lib/lxip/nic_handler.cc
+++ repos/dde_linux/src/lib/lxip/nic_handler.cc
@@ -52,6 +52,8 @@ class Nic_client
 			if (link_state == false || lxip_do_dhcp() == false)
 				return;
 
+			Lx::timer_update_jiffies();
+
 			/* reconnect dhcp client */
 			lxip_configure_dhcp();
 		}
@@ -61,6 +63,8 @@ class Nic_client
 		 */
 		void _packet_avail()
 		{
+			Lx::timer_update_jiffies();
+
 			/* process a batch of only MAX_PACKETS in one run */
 			enum { MAX_PACKETS = 20 };
 
