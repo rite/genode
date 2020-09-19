797cb6f6a2ba4371ebf5632cf3b5ece98638a10a
Roman Iten <roman.iten@gapfruit.com>
2020-09-19 18:07:57 +0200

    TEST: run/bash: poll fs_rom attached to /tmp/test.txt
    Issue #3897

---
+++ repos/os/src/test/fs_rom_delete/main.cc
@@ -0,0 +1,45 @@
+#include <base/component.h>
+#include <base/attached_rom_dataspace.h>
+
+
+namespace Test_fs_rom_delete
+{
+	using namespace Genode;
+
+	class Main;
+};
+
+
+class Test_fs_rom_delete::Main
+{
+	private:
+
+		Env&                   _env;
+		Attached_rom_dataspace _rom   { _env, "test.txt" };
+		
+		Signal_handler<Main> _handler { _env.ep(), *this, &Main::_update };
+
+		void _update() { }
+
+	public:
+
+		Main(Env& env) : _env { env }
+		{
+			/*
+			 * In case the file got deleted, the fs_rom only serves an empty ROM
+			 * if the following line is uncommented.
+			 */
+			_rom.sigh(_handler);
+
+			while (true) {
+				_rom.update();
+				log(_rom.local_addr<const char>());
+			}
+		 }
+};
+
+
+void Component::construct(Genode::Env& env)
+{
+	static Test_fs_rom_delete::Main main { env };
+}
